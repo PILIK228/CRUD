ea6189a4783d667910ee03521af8a5509602b8ac
evgenkld <evgenkld@inbox.ru>
2023-03-18 18:08:54 +0300

    refactor

--- serializer.py
+++ serializer.py
@@ -1,19 +1,32 @@
 with open('new_base.json') as DB:
     m = DB.read().split('\n')
-m = ''.join(map(lambda x: x.strip(), m))
-m = m[1:-1]
-s = {}
-k = True
+global_dict = dict()
+d = dict()
+empty_dict = dict()
 
-# for i in range(len(m)):
-#     if m[i] == '"' and k == True:
-#         sk = m[i+1]
-#         ek = m.find('"', i+1, len(m))+1
-#         k = False
-#         key = m[sk:ek]
-#     elif m[i] == '"' and k == False:
-#         k = True 
-
-
-
-print(m)
+for i in m:
+    i = i.strip()
+    if i[0] == "{":
+        # if d != empty_dict:
+            # global_dict = d
+        d = dict()
+        continue
+    if i[0] == '"':
+        ind = i.find('"', 1)
+        if i[-1] == "[":
+            global key
+            key = i[1:ind]
+            d[key] = list()
+        elif i[-1] == ",":
+            key1 = i[1:ind]
+            value1 = i[ind+4: -2]
+            d[key1] = value1
+        else:
+            key1 = i[1:ind]
+            value1 = i[ind+4: -1]
+            d[key1] = value1    
+            t = type(global_dict[key])
+            if t == list:
+                a = global_dict.get(key)
+                a.append(d)
+                
